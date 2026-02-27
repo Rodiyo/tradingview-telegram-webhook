d70002f7137153103e7c36dbbc3ede7902fdf4ad
Rodiyo <sperseu19@gmail.com>
2026-02-27 14:59:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -214,11 +214,7 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "/remove &lt;chat_id&gt; – Remove an approved user<br>"
         )
 
-    await update.message.reply_text(text, parse_mode="HTML")
-
-
-    await update.message.reply_text(text, parse_mode="HTML")
-
+      await update.message.reply_text(text, parse_mode="HTML")
 
 
 # -------------------------
